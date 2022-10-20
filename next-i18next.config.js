a7dd1f44fcb20894c7700c953d1e1d5d30dfa8fe
jzhaormg <105235704+jzhaormg@users.noreply.github.com>
2022-10-20 09:54:58 -0700

    fix another build error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -7,7 +7,7 @@ const config = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
-    localePath: path.resolve('./packages/ghost-test-frontend/public/locales')
+    localePath: 'packages/ghost-test-frontend/public/locales'
   }
 }
 
